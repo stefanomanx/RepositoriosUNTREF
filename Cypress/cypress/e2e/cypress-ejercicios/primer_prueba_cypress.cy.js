6325fff00a98fdee47a2188acf140280907562d1
Stefano <stefano232@gmail.com>
2025-10-02 19:23:50 -0300

    Reordenamiento de carpetas e instalación de Playwright

--- Cypress/cypress/e2e/cypress-ejercicios/primer_prueba_cypress.cy.js
+++ Cypress/cypress/e2e/cypress-ejercicios/primer_prueba_cypress.cy.js
@@ -1,15 +1,24 @@
 /// <reference types="cypress" />
 
-describe ("Pruebas de Cypress de UNTREF", () => {
+describe("Pruebas de Cypress de UNTREF", () => {
+    beforeEach(() => {
+        // Interceptar cualquier petición a dominios de Optimizely (incluye subdominios dinámicos)
+        cy.intercept('GET', /log\.optimizely\.com/, { statusCode: 200, body: '' }).as('Optimizely');
 
-    it("prueba de ejecucion de cypress",()=>{
+        // Interceptar cualquier endpoint que contenga /event (incluye query strings)
+        cy.intercept('GET', /\/event/, { statusCode: 200, body: '' }).as('event');
 
-    cy.visit("https://the-internet.herokuapp.com/login");
-    cy.get("#username").type("tomsmith");
-    cy.get("#password").type("SuperSecretPassword!")
+        cy.on('uncaught:exception', (err, runnable) => {
+            return false;
+        });
+    });
 
-})
-
-
-
-})
+    it("Prueba de ejecución de Cypress", () => {
+        cy.visit("https://the-internet.herokuapp.com/login");
+        cy.get("#username").type("tomsmith");
+        cy.get("#password").type("SuperSecretPassword!");
+        cy.get('button[type="submit"]').click();
+        cy.url().should("include", "/secure");
+        cy.get("#flash").should("contain.text", "You logged into a secure area!");
+    });
+});
